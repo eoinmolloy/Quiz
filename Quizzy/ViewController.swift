0898c20ad2d9299f63f8e7bb7c23a8bf4fe13c6a
Eoin Molloy <ieoinmolloy@icloud.com>
2015-12-26 15:11:14 +0000

    Initial Commit

---
+++ Quizzy/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Quizzy
+//
+//  Created by Eoin Molloy on 26/12/2015.
+//  Copyright © 2015 Eoin Molloy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
